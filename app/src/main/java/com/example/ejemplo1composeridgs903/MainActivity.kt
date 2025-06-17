ed3b4a5842662126fea97d38e5fed82c530b0da4
IDGS-903-22001440 <80439@alumnos.utleon.edu.mx>
2025-06-17 00:32:38 -0600

    Cards 10/06/25

--- app/src/main/java/com/example/ejemplo1composeridgs903/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1composeridgs903/MainActivity.kt
@@ -1,73 +1,88 @@
 package com.example.ejemplo1composeridgs903
 
-import android.media.Image
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ejemplo1composeridgs903.ui.theme.Ejemplo1ComposerIDGS903Theme
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-               Targeta()
+            Tarjeta()
         }
     }
-}
 
-
-@Composable
-fun Targeta(){
-    Row {
-        ImagenHeroe()
-        Personaje()
+    @Composable
+    fun Tarjeta(){
+        Row {
+            ImagenHeroe()
+            Personaje()
+        }
     }
-}
 
-@Composable
-fun ImagenHeroe(){
-    Image(
-        painter = painterResource(R.drawable.marcarita),
-        contentDescription = "Macarita",
-        modifier = Modifier
-            .padding(16.dp)
-            .size(100.dp)
-            .clip(CircleShape)
-            .background(Color.Gray)
 
-    )
-}
-
-@Composable
-fun Personaje(){
-    Column {
-        Text("Gohan")
-        Text("Son Goku")
+    @Composable
+    fun Personaje(){
+        Column {
+            Text("Gohan")
+            Text("Son Goku")
+            Text("Chi Chi")
+        }
     }
-}
 
-@Preview
-@Composable
-fun GrettingPreview(){
-    Personaje()
-    ImagenHeroe()
+    @Composable
+    fun ImagenHeroe(){
+        Image(
+            painter = painterResource(R.drawable.chichi),
+            contentDescription = "Chi Chi",
+            modifier = Modifier
+                .padding(16.dp)
+                .size(100.dp)
+                .clip(CircleShape)
+                .background(Color.Gray)
+
+        )
+    }
+
+    @Preview
+    @Composable
+    fun GreetingPreview(){
+        Tarjeta()
+    }
 }
